93a4c4434c98a785d585ad2b7622c2ce6f45d3e6
SyaefulBachtiar <syaefulbachtiar2@gmail.com>
2025-07-04 18:00:52 +0700

    Header dengan logo dan menu navigasi. Section utama berisi gambar dan teks deskripsi. Footer berisi link media sosial.1

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -18,12 +18,17 @@ export default function Header(){
         <div className="p-5 bg-blue-200 fixed top-0 w-full z-50 overflow-x-hidden ">
           <div className="flex">
             {/* Logo */}
-            <div className="font-inter flex items-center gap-[200px] w-[25%]">
-              <Link>
-              <img src="/images/logo.png" alt="Logo" />
+            <div className="font-inter flex items-center justify-between gap-[100px] w-full sm:w-auto">
+              <Link to="/">
+                <img
+                  src="/images/logo.png"
+                  alt="Logo"
+                  className="w-20 sm:w-28 md:w-32"
+                />
               </Link>
+
               {/* Icon menu */}
-              <div>
+              <div className="">
                 <IconBar handleSidebar={handleSidebar} />
               </div>
             </div>
